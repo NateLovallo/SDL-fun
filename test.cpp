ddb5c0a056d8f0dcfb298e7fa70c0648d7929096
Nate Lovallo <NateLovallo@users.noreply.github.com>
2020-04-18 23:06:36 -0600

    The needle SPINS

--- test.cpp
+++ test.cpp
@@ -15,60 +15,75 @@ int main(int argc, char* args[])
    
    SDL_ShowCursor(SDL_DISABLE);
    
-   SDL_Window* window = window = SDL_CreateWindow( "SDL Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 600, 400, SDL_WINDOW_SHOWN );
+   SDL_Window* window = window = SDL_CreateWindow( "SDL Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 400, SDL_WINDOW_SHOWN );
    if (window == NULL)
    {
       fprintf(stderr,"ERROR in SDL_CreateWindow(): %s\n",SDL_GetError());
       return -1;
    }
-   
-   // Get window surface
-   screenSurface = SDL_GetWindowSurface( window );
 
-   // Fill the surface black
-   SDL_FillRect( screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0, 0, 0 ));
-   
-   // Draw a green box
-
-   //SDL_FillRect(screenSurface,&rectBox,SDL_MapRGB( screenSurface->format, 0, 255, 0 ));
-   
-   
-   
-   SDL_Surface* gauge = IMG_Load("../gauge.png");
-  
-   if (gauge == NULL)
+   SDL_Renderer* myRenderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
+   if( myRenderer == NULL )
    {
-      fprintf(stderr,"Coudln't load image: %s\n",SDL_GetError());
+      fprintf(stderr,"Coudln't make renderino %s\n",SDL_GetError());
       return -1;
    }
-   SDL_Surface* gaugeOptimized = SDL_ConvertSurface( gauge, screenSurface->format, 0 );
-   SDL_FreeSurface( gauge );
    
-   
-   SDL_Surface* needle = IMG_Load("../needle.png");
-  
-   if (needle == NULL)
+   int imgFlags = IMG_INIT_PNG;
+   if( !( IMG_Init( imgFlags ) & imgFlags ) )
    {
-      fprintf(stderr,"Coudln't load image: %s\n",SDL_GetError());
-      return -1;
+   printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+   return -1;
    }
-   SDL_Surface* needleOptimized = SDL_ConvertSurface( needle, screenSurface->format, 0 );
-   SDL_FreeSurface( needle );
-   
-   
-   
-   
-   
-   SDL_Rect rectBg = {0,0,600,400};
-   SDL_FillRect(screenSurface,&rectBg,SDL_MapRGB( screenSurface->format, 0, 0, 0 ));
-   
-   SDL_BlitSurface( gaugeOptimized, NULL, screenSurface, NULL );
-   
-   SDL_BlitSurface( needleOptimized, NULL, screenSurface, NULL );
-   
 
 
+   SDL_Texture* gauge = NULL;
+   {
+      //Load image at specified path
+      SDL_Surface* loadedSurface = IMG_Load( "../gauge.png" );
+      if( loadedSurface == NULL )
+      {
+         printf( "Unable to load image! SDL_image Error: %s\n", IMG_GetError() );
+         return -1;
+      }
+      
+      //Create texture from surface pixels
+      gauge = SDL_CreateTextureFromSurface( myRenderer, loadedSurface );
+      if( gauge == NULL )
+      {
+         printf( "Unable to create texture SDL Error: %s\n", SDL_GetError() );
+      }
+
+      //Get rid of old loaded surface
+      SDL_FreeSurface( loadedSurface );
+   }
+   
+   
+   SDL_Texture* needle = NULL;
+   {
+      //Load image at specified path
+      SDL_Surface* loadedSurface = IMG_Load( "../needle.png" );
+      if( loadedSurface == NULL )
+      {
+         printf( "Unable to load image! SDL_image Error: %s\n", IMG_GetError() );
+         return -1;
+      }
+      
+      //Create texture from surface pixels
+      needle = SDL_CreateTextureFromSurface( myRenderer, loadedSurface );
+      if( needle == NULL )
+      {
+         printf( "Unable to create texture SDL Error: %s\n", SDL_GetError() );
+      }
+
+      //Get rid of old loaded surface
+      SDL_FreeSurface( loadedSurface );
+   }
+   
+   
    bool run = true;
+   
+   double rotate = 0.0;
    
    while (run)
    {
@@ -85,13 +100,34 @@ int main(int argc, char* args[])
          }
       }
       
-      // Update the surface
-      SDL_UpdateWindowSurface( window );
-      
-   }
+      SDL_SetRenderDrawColor(myRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+      SDL_RenderClear( myRenderer );
 
-   //SDL_DestroyRenderer(renderer);
-   //SDL_DestroyWindow(window);
+      SDL_RenderCopy( myRenderer, gauge, NULL, NULL );
+      
+      SDL_Rect rectNeedle = {200-10,200,20,200};
+      SDL_Point rPoint = {10,0};
+      SDL_RenderCopyEx( myRenderer, needle, NULL, &rectNeedle, rotate, &rPoint, SDL_FLIP_VERTICAL);
+      rotate += 1.0;
+      
+      if (rotate >= 360.0)
+      {
+         rotate = 0.0;
+      }
+      
+      //SDL_SetRenderDrawColor(myRenderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+      //SDL_RenderDrawLine(myRenderer, 200, 200, 205, 0);
+
+      SDL_SetRenderDrawColor(myRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+      
+      SDL_RenderPresent(myRenderer);
+   }
+   
+   
+   
+   SDL_DestroyTexture( gauge );
+   SDL_DestroyRenderer(myRenderer);
+   SDL_DestroyWindow(window);
 
 
    TTF_Quit();
